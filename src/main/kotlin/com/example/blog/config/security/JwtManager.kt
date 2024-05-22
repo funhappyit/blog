ed242ba8ec91cf8d15b79e16c43cfc2874b7b294
한유경 <yugyeong.han@seculayer.com>
2024-05-22 17:36:18 +0900

    2024-05-21 1) 스프링 시큐리티 적용 jwt 설정

--- src/main/kotlin/com/example/blog/config/security/JwtManager.kt
+++ src/main/kotlin/com/example/blog/config/security/JwtManager.kt
@@ -5,21 +5,33 @@ import com.auth0.jwt.algorithms.Algorithm
 import mu.KotlinLogging
 import java.security.Principal
 import java.util.*
+import javax.crypto.SecretKey
 
 class JwtManager {
 
     private val log = KotlinLogging.logger {}
 
+    private val secretKey: String = "SFSDFSDF"
+    private val claimEmail = "email"
+    private val claimPassword = "password"
+    private val expireTime = 1000*60*60
+
+
     fun generateToken(principal:PrincipalDetails) {
 
         val sign = JWT.create()
             .withSubject(principal.username)
-            .withExpiresAt(Date(System.nanoTime()+1000*60*60))
+            .withExpiresAt(Date(System.nanoTime()+expireTime))
             .withClaim("email",principal.username)
             .withClaim("password",principal.password)
-            .sign(Algorithm.HMAC512("SFSDFSDF"))
+            .sign(Algorithm.HMAC512(secretKey))
 
     }
+
+    fun getMemberEmail(token:String) : String? {
+        return JWT.require(Algorithm.HMAC512(secretKey)).build().verify(token).getClaim("email").asString()
+    }
+
 
 
 
